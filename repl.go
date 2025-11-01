651da0f7d7cc765d01b927464cf3c33d41153def
Max I. <clickkava@gmail.com>
2025-11-01 15:50:01 +0200

    Help and Exit

--- repl.go
+++ repl.go
@@ -7,6 +7,76 @@ import (
 		"os"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		scanner.Scan()           // Wait for user input
+		input := scanner.Text()  // Get the input as a string
+		cleanInput := cleanInput(input)
+
+		if len(cleanInput) == 0 {
+			continue
+		}
+
+		command := cleanInput[0]
+
+		cmd, exists := getCommands()[command]  // Try to get the command from map
+		if exists {                            // Did we find it?
+			err := cmd.callback()       		 		 // Call the function stored in callback
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+
+func commandHelp() error {
+	fmt.Println()
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	fmt.Println()
+	return nil
+}
+
 func cleanInput(text string) []string {
 	// Convert to lowercase
 	lower := strings.ToLower(text)
@@ -18,16 +88,4 @@ func cleanInput(text string) []string {
 	words := strings.Fields(trimmed)
 
 	return words
-}
-
-func startRepl() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()           // Wait for user input
-		input := scanner.Text()  // Get the input as a string
-		cleanInput := cleanInput(input)
-		fmt.Println("Your command was:", cleanInput[0])
-	}
 }
